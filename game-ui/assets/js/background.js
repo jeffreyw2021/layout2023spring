cfe6e5a0ffd27795b874dd97e73da733fca80299
jeffrey <jeffrey.w@wustl.edu>
2023-04-04 11:51:33 -0500

    panel style update

--- game-ui/assets/js/background.js
+++ game-ui/assets/js/background.js
@@ -6,18 +6,28 @@ let acceleration = 0.0001;
 let bikeSpeed = 5.98; // meters per second
 
 function moveBackground() {
-  marginLeft -= speed;
-  layerTop.style.backgroundPosition = `${marginLeft}px bottom`;
-  speed += acceleration;
-  bikeSpeed = speed * 5.98 / 60;
+  if (speed <= 10) {
+    marginLeft -= speed;
+    layerTop.style.backgroundPosition = `${marginLeft}px bottom`;
+    speed += acceleration;
+    bikeSpeed = speed * 5.98 / 60;
 
-  if (speed >= 8) {
-    const red = document.querySelector('#red');
-    red.style.animation = 'wobble 5s infinite alternate, redBackForth 5s infinite linear';
-  }
-  if (speed >= 8.5) {
-    const white = document.querySelector('#white');
-    white.style.animation = 'wobble 5s infinite alternate, whiteBackForth 5s infinite linear';
+    if (speed >= 7) {
+      const purple = document.querySelector('#purple');
+      purple.style.animation = 'wobble 5s infinite alternate, purpleBackForth 5s infinite linear';
+    }
+    if (speed >= 8) {
+      const red = document.querySelector('#red');
+      red.style.animation = 'wobble 5s infinite alternate, redBackForth 5s infinite linear';
+    }
+    if (speed >= 8.2) {
+      const white = document.querySelector('#white');
+      white.style.animation = 'wobble 5s infinite alternate, whiteBackForth 5s infinite linear';
+    }
+  }else{
+    marginLeft -= speed;
+    layerTop.style.backgroundPosition = `${marginLeft}px bottom`;
+    bikeSpeed = speed * 5.98 / 60;
   }
 }
 
